3468c7f3d5bc1e8887fc35c851cc90ed3176ca29
bellmmm <91827558+bellmmm@users.noreply.github.com>
2021-10-20 13:38:57 +0000

    remove and input url

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+
+
 import './App.css';
+import useSWR from 'swr'
+
+const fetcher = (url: string) => fetch(url).then(res => res.json())
 
 function App() {
+
+    const { data, error } = useSWR(
+      `https://data.nasdaq.com/api/v3/datasets/ODA/PIORECR_USD.json?start_date=2016-01-01&api_key=VJivxkw2oj1AJMb8DPgq`,
+      fetcher)
+
+    if (error) return <div>failed to load</div>
+    if (!data) return <div>loading...</div>
+
+    // let results = {
+  //     : data.location.country,
+  //     cityName: data.location.name,
+  //     temperature: data.current.temp_c,
+  //     conditionText: data.current.condition.text,
+  //     icon: data.current.condition.icon
+    // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>こんにちは{JSON.stringify(data)}</h1>
+
     </div>
   );
 }
-
 export default App;
